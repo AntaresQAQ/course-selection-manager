16e41784911105dbc2e52819626e152783994da6
Antares <antares.oier@gmail.com>
2021-07-06 21:21:01 +0800

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SharedModule } from './shared.module';
+import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
+import { SelectionModule } from './selection/selection.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [SharedModule, forwardRef(() => DatabaseModule)],
+  imports: [
+    SharedModule,
+    forwardRef(() => DatabaseModule),
+    forwardRef(() => AdminModule),
+    forwardRef(() => StudentModule),
+    forwardRef(() => SelectionModule),
+    forwardRef(() => CourseModule),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
